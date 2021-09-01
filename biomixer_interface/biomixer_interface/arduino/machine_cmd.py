5ffc8260e1b688d2de8f91cad69fec19f51c559c
Gonzalo Olave <growolff@gmail.com>
2021-09-01 13:20:39 -0400

    added command value

--- biomixer_interface/biomixer_interface/arduino/machine_cmd.py
+++ biomixer_interface/biomixer_interface/arduino/machine_cmd.py
@@ -1,17 +1,39 @@
 # -*- coding: utf-8 -*
 
 import struct
+import serial
+
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
+
+def write(x):
+    return arduino.write(x)
+
+def read():
+    return arduino.readline()
+
+
+class Device(object):
+
+    def __init__(self):
+        self.dev = serial.Serial('/dev/ttyACM0',9600,timeout=10)
+
 
 class MachineCmd(object):
     """
-        define the structure of the data, little-endian
-        h is for 2 byte integer, see the full list here https://docs.python.org/3/library/struct.html#format-characters
-        add as many data types as you want to send to arduino
+        8bit commands for controlling the machine
     """
+    CMD_SET_VALUES = 5
+    CMD_START = 1
 
     def __init__(self):
-        self.cmd_struct = struct.Struct('<hhhhh')
+        """
+            define the structure of the data, little-endian
+            add as many data types as you want to send to arduino
+            h is for 2 byte integer, see the full list here https://docs.python.org/3/library/struct.html#format-characters
+        """
+        self.cmd_struct = struct.Struct('<bhhhhh')
         self.packet = ''
+        self.cmd = 0
         self.d1 = 0
         self.d2 = 0
         self.d3 = 0
@@ -19,12 +41,13 @@ class MachineCmd(object):
         self.d5 = 0
 
     def serialize(self):
-        self.packet = self.cmd_struct.pack(self.d1, self.d2, self.d3, self.d4, self.d5)
+        self.packet = self.cmd_struct.pack(self.cmd,self.d1, self.d2, self.d3, self.d4, self.d5)
 
     def to_hex(self):
         return self.packet.hex()
 
-    def set_values(self, d1=0, d2=0, d3=0, d4=0, d5=0):
+    def set_values(self, cmd=0, d1=0, d2=0, d3=0, d4=0, d5=0):
+        self.cmd = cmd
         self.d1 = d1
         self.d2 = d2
         self.d3 = d3

cf6bb53e82b48e6caeaa2c382f392cb5ce938ed7
Gonzalo Olave <growolff@gmail.com>
2021-09-03 13:14:07 -0400

    arduino machine class

--- biomixer_interface/biomixer_interface/arduino/machine_cmd.py
+++ biomixer_interface/biomixer_interface/arduino/machine_cmd.py
@@ -3,20 +3,6 @@
 import struct
 import serial
 
-arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
-
-def write(x):
-    return arduino.write(x)
-
-def read():
-    return arduino.readline()
-
-
-class Device(object):
-
-    def __init__(self):
-        self.dev = serial.Serial('/dev/ttyACM0',9600,timeout=10)
-
 
 class MachineCmd(object):
     """
@@ -25,12 +11,13 @@ class MachineCmd(object):
     CMD_SET_VALUES = 5
     CMD_START = 1
 
-    def __init__(self):
+    def __init__(self,port='/dev/ttyACM0'):
         """
             define the structure of the data, little-endian
             add as many data types as you want to send to arduino
             h is for 2 byte integer, see the full list here https://docs.python.org/3/library/struct.html#format-characters
         """
+
         self.cmd_struct = struct.Struct('<bhhhhh')
         self.packet = ''
         self.cmd = 0
@@ -39,6 +26,13 @@ class MachineCmd(object):
         self.d3 = 0
         self.d4 = 0
         self.d5 = 0
+
+    def portIsUsable(self,portName='/dev/ttyACM0'):
+        try:
+           self.arduino = serial.Serial(port,9600,timeout=10)
+           return True
+        except:
+           return False
 
     def serialize(self):
         self.packet = self.cmd_struct.pack(self.cmd,self.d1, self.d2, self.d3, self.d4, self.d5)
@@ -53,6 +47,11 @@ class MachineCmd(object):
         self.d3 = d3
         self.d4 = d4
         self.d5 = d5
+
+        self.serialize()
+
+    def write(self):
+        self.arduino.write(self.packet)
 
     def read(self, line=False, size=1):
         if line is True:
